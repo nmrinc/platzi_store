3265acac844938bdf559b73aa5ea2386a027e2b1
Erick Avila <nmr.inc@icloud.com>
2021-03-23 20:47:01 -0600

    Lesson 10. Snapshot

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # platzi-store
+
 React Unit tests with Jest Course
 
 ---
@@ -6,15 +7,24 @@ React Unit tests with Jest Course
 ## Basic Jest dependencies
 
 ´
-npm i -D jest enzyme enzyme-adapter-react-16
+npm i -D jest enzyme enzyme-adapter-react-16 react-test-renderer
 ´
 
 ## Jest
+
 Jest is a delightful JavaScript Testing Framework with a focus on simplicity.
 
 ## enzyme
+
 Enzyme is a JavaScript Testing utility for React that makes it easier to test your React Components' output. You can also manipulate, traverse, and in some ways simulate runtime given the output.
 
 ## enzyme React adapter
+
 It changes depending on the version of React you're using.
 It's needed to use React configurations on tests.
+
+## react-test-renderer
+
+This package provides an experimental React renderer that can be used to render React components to pure JavaScript objects, without depending on the DOM or a native mobile environment.
+
+Essentially, this package makes it easy to grab a snapshot of the "DOM tree" rendered by a React DOM or React Native component without using a browser or jsdom.
--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
@@ -15,3 +16,26 @@ describe('<Footer />', () => {
   });
 
 });
+
+/**
+ * @concept Snapshot
+ * @context When a component won't change gradually, you can take a snapshot to compare it and know if there was changes
+ * @o As the snapshot needs to be a json file. Will use the react-test-renderer dependency to convert the component to plain Js.
+*/
+describe('Footer Snapshot', () => {
+  test('Check UI of <Footer />', () => {
+    const footer = create(<Footer />);
+
+    expect(footer.toJSON()).toMatchSnapshot();
+  });
+});
+
+/**
+ * @concept Update Snapshot
+ * @context When there's a previous snapshot, but a change it's required. You can update the snapshot with the change so can pass the test
+ * @a You need to run the next codes
+ * @tst >jest --updateSnapshot
+ * ----------------------------------------------------------------
+ * @o Additionally, you can add the script to package
+ * @tst "test:update": "jest --updateSnapshot"
+*/
